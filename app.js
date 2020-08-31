bd08598b5daabfdae914be183bd26ee95aa8467c
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-08-31 11:38:58 -0700

    Implement nested React components

--- app.js
+++ app.js
@@ -4,11 +4,23 @@
 var GroceryList = () => (
   <div>
     <ul>
-      <li>Fruit</li>
-      <li>Spices</li>
+      <Fruit />
+      <Spices />
     </ul>
   </div>
 );
 
+//Create React components for the 2 items in your grocery list. Create a Fruit component and a Spices component
+var Fruit = () => (
+  <li>Fruit</li>
+);
+
+//Note: had a helpdesk ticket because I used {} instead of (). Note bracket usage on arrow functions means I need to use a return statement, whereas with () the return is auto generated.
+
+
+var Spices = () => (
+  <li>Spices</li>
+);
+
 //Render this component to the div tag inside index.html with an id of app
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+ReactDOM.render(<GroceryList />, document.getElementById('app'));

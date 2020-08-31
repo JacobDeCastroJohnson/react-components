292e478fb52b73e6a7c52df5c240a276abf9d7b6
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-08-31 11:00:53 -0700

    Implement creating and rendering component for App

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // TODO
+
+//Create a GroceryList component that contains an unordered list of 2 grocery items
+var GroceryList = () => (
+  <div>
+    <ul>
+      <li>Fruit</li>
+      <li>Spices</li>
+    </ul>
+  </div>
+);
+
+//Render this component to the div tag inside index.html with an id of app
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
